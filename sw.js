793198cfc6eaf62435ddf1ff0d77d52e989949f4
staticover <onstrahq@gmail.com>
2026-03-16 18:13:03 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'arko-v1';
+const ASSETS = [
+  '/arko/',
+  '/arko/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for API calls, cache first for assets
+  if (e.request.url.includes('supabase.co')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('{}', {headers:{'Content-Type':'application/json'}})));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
